d54e74ef32b225420965edade52e128f3bdb7ae4
David Patterson <davepatterson12@gmail.com>
2014-10-12 10:29:06 -0500

    Initial Commit

---
+++ Token/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Token
+//
+//  Created by Dave on 10/12/14.
+//  Copyright (c) 2014 Black Box Creative. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Token/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Token
+//
+//  Created by Dave on 10/12/14.
+//  Copyright (c) 2014 Black Box Creative. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
