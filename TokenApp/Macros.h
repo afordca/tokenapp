e264862255cedaf1fa66e320c829598677b1eb4d
Basel Farag <basel.farag@gmail.com>
2014-12-14 11:55:53 -0600

    Bunch of stuff added. UI still ugly. Working on it.

---
+++ TokenApp/Macros.h
@@ -0,0 +1,13 @@
+//
+//  Macros.h
+//  TokenApp
+//
+//  Created by BASEL FARAG on 12/13/14.
+//  Copyright (c) 2014 ABaselNotBasilProduction. All rights reserved.
+//
+
+#ifndef TokenApp_Macros_h
+#define TokenApp_Macros_h
+
+
+#endif
---
+++ TokenApp/NavTabBarController.h
@@ -0,0 +1,13 @@
+//
+//  NavTabBarController.h
+//  TokenApp
+//
+//  Created by BASEL FARAG on 12/13/14.
+//  Copyright (c) 2014 ABaselNotBasilProduction. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NavTabBarController : UITabBarController
+
+@end
