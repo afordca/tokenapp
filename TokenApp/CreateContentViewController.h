76dc85a7fd8dd49cb24c507bdc038efc92ec1188
Basel Farag <basel.farag@gmail.com>
2014-12-17 00:07:24 -0600

    Setting up content generators, initial code for creating Content

---
+++ TokenApp/CreateContentViewController.h
@@ -0,0 +1,13 @@
+//
+//  CreateContentViewController.h
+//  TokenApp
+//
+//  Created by BASEL FARAG on 12/17/14.
+//  Copyright (c) 2014 ABaselNotBasilProduction. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CreateContentViewController : UIViewController
+
+@end
