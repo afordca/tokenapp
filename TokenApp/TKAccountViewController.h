8cc405f25df1e48c9ac69fedcd209e6c63628b1e
Basel Farag <basel.farag@gmail.com>
2014-12-24 23:40:05 -0600

    More refactoring. Reorganization of Home Section including profile, homeview, and contenttimeline

---
+++ TokenApp/TKAccountViewController.h
@@ -0,0 +1,13 @@
+//
+//  TKAccountViewController.h
+//  
+//
+//  Created by BASEL FARAG on 12/24/14.
+//
+//
+
+#import "PFQueryTableViewController.h"
+
+@interface TKAccountViewController : PFQueryTableViewController
+
+@end
---
+++ TokenApp/TKHomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  TKHomeViewController.h
+//  TokenApp
+//
+//  Created by BASEL FARAG on 12/24/14.
+//  Copyright (c) 2014 ABaselNotBasilProduction. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TKHomeViewController : UIViewController
+
+@end
