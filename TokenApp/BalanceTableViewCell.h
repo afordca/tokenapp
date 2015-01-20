d1b6a838af74b0368a70793a61b09fc2e3222c93
Basel Farag <basel.farag@gmail.com>
2015-01-20 11:45:57 -0600

    continued prototype work; build back to function

---
+++ TokenApp/BalanceTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  BalanceTableViewCell.h
+//  TokenApp
+//
+//  Created by BASEL FARAG on 1/20/15.
+//  Copyright (c) 2015 ABaselNotBasilProduction. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BalanceTableViewCell : UITableViewCell
+
+@end
---
+++ TokenApp/BalanceTableViewCell.m
@@ -0,0 +1,13 @@
+//
+//  BalanceTableViewCell.m
+//  TokenApp
+//
+//  Created by BASEL FARAG on 1/20/15.
+//  Copyright (c) 2015 ABaselNotBasilProduction. All rights reserved.
+//
+
+#import "BalanceTableViewCell.h"
+
+@implementation BalanceTableViewCell
+
+@end
