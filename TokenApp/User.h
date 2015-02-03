ad77fa6504c589572a845c71a3c0847dd21f08b7
Basel Farag <basel.farag@gmail.com>
2015-02-03 14:17:50 -0600

    Provisioning Profile/User Singleton/UIBranch Changes

--- TokenApp/User.h
+++ TokenApp/User.h
@@ -7,7 +7,20 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
 
 @interface User : NSObject
+{
+    UIImage *profileImage;
+    NSString *userName;
+
+}
+
+@property (nonatomic, strong) UIImage *profileImage;
+@property (nonatomic,strong) NSString *userName;
+
+// This is the method to access this Singleton class
++ (User *)sharedSingleton;
 
 @end
--- TokenApp/User.m
+++ TokenApp/User.m
@@ -7,7 +7,30 @@
 //
 
 #import "User.h"
+#import <Parse/Parse.h>
 
 @implementation User
+{
+    User *anotherSingle;
+}
+
+
+@synthesize profileImage;
+@synthesize userName;
+
+
++ (User *)sharedSingleton
+{
+    static User *single = nil;
+    {
+        if ( !single)
+        {
+            // allocate the shared instance, because it hasn't been done yet
+            single = [[User alloc] init];
+        }
+
+        return single;
+    }
+}
 
 @end
