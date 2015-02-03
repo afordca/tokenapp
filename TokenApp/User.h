bdac52e997a3fc871d98a86b1fe154417bcec70a
Basel Farag <basel.farag@gmail.com>
2015-02-03 14:17:46 -0600

    Provisioning Profile/User Singleton/UIBranch Changes

---
+++ TokenApp/User.h
@@ -0,0 +1,13 @@
+//
+//  User.h
+//  
+//
+//  Created by BASEL FARAG on 2/3/15.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface User : NSObject
+
+@end
---
+++ TokenApp/User.m
@@ -0,0 +1,13 @@
+//
+//  User.m
+//  
+//
+//  Created by BASEL FARAG on 2/3/15.
+//
+//
+
+#import "User.h"
+
+@implementation User
+
+@end
