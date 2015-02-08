14ce1095b02a8a6495a357fb9009a0060bb607b2
Basel Farag <basel.farag@gmail.com>
2015-02-08 00:28:23 -0600

    homefeedviewcontroller work

--- TokenApp/MFC_HomeFeedViewController.m
+++ TokenApp/MFC_HomeFeedViewController.m
@@ -7,9 +7,14 @@
 //
 
 #import "MFC_HomeFeedViewController.h"
+#import "Constants.h"
+
 
 @interface MFC_HomeFeedViewController ()<UITableViewDataSource, UITableViewDelegate>
 @property (strong, nonatomic) IBOutlet UITableView *tableViewHomeFeed;
+@property (nonatomic, assign) BOOL shouldReloadOnAppear;
+@property (nonatomic, strong) NSMutableSet *reusableSectionHeaderViews;
+@property (nonatomic, strong) NSMutableDictionary *outstandingSectionHeaderQueries;
 
 @end
 
@@ -18,6 +23,33 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+}
+
+- (id)initWithStyle:(UITableViewStyle)style
+{
+    self = [super initWithStyle:style];
+    if (self) {
+
+        self.outstandingSectionHeaderQueries = [NSMutableDictionary dictionary];
+
+        // The className to query on
+        self.className = kPAPPhotoClassKey;
+
+        // Whether the built-in pull-to-refresh is enabled
+        self.pullToRefreshEnabled = YES;
+
+        // Whether the built-in pagination is enabled
+        self.paginationEnabled = YES;
+
+        // The number of objects to show per page
+        self.objectsPerPage = 10;
+
+        // Improve scrolling performance by reusing UITableView section headers
+        self.reusableSectionHeaderViews = [NSMutableSet setWithCapacity:3];
+
+        self.shouldReloadOnAppear = NO;
+    }
+    return self;
 }
 
 #pragma mark - UITableView Delegate Methods
@@ -34,5 +66,16 @@
 }
 
 -(IBAction)unwindToHomeFeed:(UIStoryboard*)sender{}
+
+#pragma mark - PFQueryTableViewcontroller 
+
+- (PFQuery *)queryForTable {
+    if (![PFUser currentUser]){
+        PFQuery *query = [PFQuery queryWithClassName:self.className];
+        [query ]
+
+
+    }
+}
 
 @end
