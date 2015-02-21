85e3c8106d74d53091f71d9b77512f1b0823d422
Basel Farag <basel.farag@gmail.com>
2015-02-20 18:06:38 -0600

    Update

---
+++ TokenApp/TKVideoCell.m
@@ -0,0 +1,21 @@
+//
+//  TKVideoCell.m
+//  TokenApp
+//
+//  Created by BASEL FARAG on 2/20/15.
+//  Copyright (c) 2015 ABaselNotBasilProduction. All rights reserved.
+//
+
+#import "TKVideoCell.h"
+
+@implementation TKVideoCell
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
