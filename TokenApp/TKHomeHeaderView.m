cf6292878a6f5b046b2a27476a377e0213a2124b
Basel Farag <basel.farag@gmail.com>
2015-02-23 16:32:52 -0600

    Work on ProfileImageView and HomeHeaderView

---
+++ TokenApp/TKHomeHeaderView.m
@@ -0,0 +1,23 @@
+//
+//  TKHomeHeaderView.m
+//  TokenApp
+//
+//  Created by BASEL FARAG on 2/23/15.
+//  Copyright (c) 2015 ABaselNotBasilProduction. All rights reserved.
+//
+
+#import "TKHomeHeaderView.h"
+
+@implementation TKHomeHeaderView
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
