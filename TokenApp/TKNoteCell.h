e2013fcfc1266980ba4c642e4179870c89a2c9ae
Basel Farag <basel.farag@gmail.com>
2015-02-23 11:13:37 -0600

    continued work on video player

---
+++ TokenApp/TKNoteCell.h
@@ -0,0 +1,13 @@
+//
+//  TKNoteCell.h
+//  TokenApp
+//
+//  Created by BASEL FARAG on 2/20/15.
+//  Copyright (c) 2015 ABaselNotBasilProduction. All rights reserved.
+//
+
+#import "PFTableViewCell.h"
+
+@interface TKNoteCell : PFTableViewCell
+
+@end
---
+++ TokenApp/TKNoteCell.m
@@ -0,0 +1,21 @@
+//
+//  TKNoteCell.m
+//  TokenApp
+//
+//  Created by BASEL FARAG on 2/20/15.
+//  Copyright (c) 2015 ABaselNotBasilProduction. All rights reserved.
+//
+
+#import "TKNoteCell.h"
+
+@implementation TKNoteCell
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
