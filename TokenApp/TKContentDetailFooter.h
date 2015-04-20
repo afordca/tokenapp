507771429459cb3c1e8e359d0e4e71a09199fc43
Basel Farag <basel.farag@gmail.com>
2015-04-20 16:35:54 -0500

    Like/Comment Footer

---
+++ TokenApp/TKContentDetailFooter.h
@@ -0,0 +1,13 @@
+//
+//  TKContentDetailFooter.h
+//  TokenApp
+//
+//  Created by BASEL FARAG on 4/20/15.
+//  Copyright (c) 2015 ABaselNotBasilProduction. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TKContentDetailFooter : UIView
+
+@end
---
+++ TokenApp/TKContentDetailFooter.m
@@ -0,0 +1,13 @@
+//
+//  TKContentDetailFooter.m
+//  TokenApp
+//
+//  Created by BASEL FARAG on 4/20/15.
+//  Copyright (c) 2015 ABaselNotBasilProduction. All rights reserved.
+//
+
+#import "TKContentDetailFooter.h"
+
+@implementation TKContentDetailFooter
+
+@end
